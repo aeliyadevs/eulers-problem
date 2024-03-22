35f44de59c7ba37438817851b072d91d683ae0b9
aeliyadevs <aeliyadevs@gmail.com>
2024-03-22 22:47:14 +0545

    solve question 8

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Globalization;
+
+class Program
 {
     static void Main()
     {
@@ -8,7 +10,8 @@
         //Question4();
         //Question5();
         //Question6();
-        Question7();
+        //Question7();
+        Question8();
     }
 
     public static void Question1()
@@ -243,5 +246,44 @@
             }
         }
         Console.WriteLine(primeCount + "st prime number is " + targetPrime);
+    }
+
+    public static void Question8()
+    {
+        Console.WriteLine("Question 8");
+        string num = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+
+        string subStr;
+        char[] arr;
+
+        int digitCount = 13;
+
+        double product = 1;
+
+        double largestProduct = 0;
+        string largestProductAdjacentDigits = "";
+
+        for (int i=0; i<(num.Length - digitCount); i++)
+        {
+            product = 1;
+            subStr = num.Substring(i, digitCount);
+            arr = subStr.ToCharArray();
+
+            foreach (char c in arr)
+            {
+                //Console.WriteLine(char.GetNumericValue(c));
+                product *= char.GetNumericValue(c);
+                //Console.WriteLine(c);
+                //Console.WriteLine(product);
+            }
+            if (product > largestProduct)
+            {
+                //Console.WriteLine(true);
+                largestProduct = product;
+                largestProductAdjacentDigits = subStr;
+            }
+        }
+        Console.WriteLine("largest Product 4 Adjacent Digits = " + largestProductAdjacentDigits);
+        Console.WriteLine("largestProduct = " + largestProduct);
     }
 }
